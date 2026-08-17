d322bb65a142cfe7898f05b82d89ef5259c3caa7
prats0007-ui <pratik.vimalbalaji@gmail.com>
2026-08-17 15:14:37 +0530

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.java
@@ -0,0 +1,31 @@
+public class Solution {
+    public boolean isPalindrome(String s) {
+        
+        int i=0,j=s.length()-1;
+        
+        while(i<j){ 
+
+            while(i<j && !isAlpha(s.charAt(i))) { 
+                i++;
+            }
+            while(j>i && !isAlpha(s.charAt(j))) {
+                j--;
+            }
+
+            if(Character.toLowerCase(s.charAt(i))!=Character.toLowerCase(s.charAt(j))){ 
+                return false; 
+            }
+
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    public boolean isAlpha(Character c){ 
+        return ((c>='A') && (c<='Z') || 
+                (c>='a') && (c<='z') || 
+                (c>='0')  && (c<='9')
+        );
+    }
+}
