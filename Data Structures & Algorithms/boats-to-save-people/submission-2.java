c0e8dd3f3abbfbef69f0127e69cffba0cf7dac4e
prats0007-ui <pratik.vimalbalaji@gmail.com>
2026-09-05 16:09:24 +0530

    Add: boats-to-save-people - submission-2

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-2.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people); 
+        int l=0,r=people.length-1,res=0; 
+
+        while(l<=r){ 
+            if(people[l]+people[r]<=limit){ 
+                res++; 
+                l++; 
+                r--;
+            }
+            else{ 
+                res++; 
+                r--;
+            }
+        }
+        return res;
+    }
+}
