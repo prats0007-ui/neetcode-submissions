815117b17f088fc7faa001004aef4d9fede66efe
prats0007-ui <pratik.vimalbalaji@gmail.com>
2026-09-07 18:10:58 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int max=0,min=prices[0];
+        for(int i=0;i<prices.length;i++){ 
+            
+                max=Math.max(max,prices[i]-min); 
+                
+                min=Math.min(min,prices[i]);
+            
+        }
+        return max;
+    }
+
+}
