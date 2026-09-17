701364ac6c4b1583b6ccf7d97b4d10332693585d
prats0007-ui <pratik.vimalbalaji@gmail.com>
2026-09-18 04:19:51 +0530

    Add: asteroid-collision - submission-2

---
+++ Data Structures & Algorithms/asteroid-collision/submission-2.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+       Stack<Integer> stack=new Stack<>(); 
+
+       for(int a:asteroids){
+        while(!stack.isEmpty() && a<0 && stack.peek()>0){
+            int diff=a+stack.peek(); 
+            if(diff<0){ 
+                stack.pop();
+            }  
+            else if(diff>0){ 
+                a=0;
+            }
+            else{ 
+                stack.pop(); 
+                a=0;
+            }
+        }
+        if(a!=0){ 
+            stack.push(a);
+        }
+       }
+
+       return stack.stream().mapToInt(i->i).toArray();
+    }
+}
